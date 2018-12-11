789500574f9d843103d2c40053a4c9d39415cd15
Daniel <braunstein93@gmail.com>
2018-12-11 22:50:13 +0200

    update experiment

--- experiment.py
+++ experiment.py
@@ -1,134 +1,55 @@
-# TODO change
 import dynet as dy
 import numpy as np
 
-global LAYERS, INPUT_DIM, HIDDEN_DIM, vocab, VOCAB_SIZE, OUTPUT_DIM, num_epochs
-
-LAYERS = 2
-INPUT_DIM = 30
-HIDDEN_DIM = 35
-vocab = ['a', 'b', 'c', 'd', 'start', 'end', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-VOCAB_SIZE = len(vocab)
-OUTPUT_DIM = 2
-num_epochs = 20
-
-model = dy.Model()
-
-lstm = dy.LSTMBuilder(LAYERS, INPUT_DIM, HIDDEN_DIM, model)
-
-lookup = model.add_lookup_parameters((VOCAB_SIZE, INPUT_DIM))
-
-# MLP after LSTM outputs
-W_mlp = model.add_parameters((OUTPUT_DIM, HIDDEN_DIM))
-b_mlp = model.add_parameters(OUTPUT_DIM)
+# usage:
+VOCAB_SIZE = 1000
+EMBED_SIZE = 100
+EPOCH = 10
 
 
-def read_data(file_name):
-    data = []
-    row = []
-    for line in file(file_name):
-        text = line.strip()
-        data.append(text)
-    return data
+# acceptor LSTM
+class LstmAcceptor(object):
+    def __init__(self, in_dim, lstm_dim, out_dim, model):
+        self.builder = dy.VanillaLSTMBuilder(1, in_dim, lstm_dim, model)
+        self.W = model.add_parameters((out_dim, lstm_dim))
+
+    def __call__(self, sequence):
+        lstm = self.builder.initial_state()
+        W = self.W.expr()  # convert the parameter into an Expession (add it to graph)
+        outputs = lstm.transduce(sequence)
+        result = W * outputs[-1]
+        return result
 
 
-def make_data_context(data, w2i):
-    contexts = []
-    for row in data:
-        curr_tag, curr_word = row.split(' ')
-        context = []
-        if curr_tag == 'good':
-            curr_tag = 1
-        elif curr_tag == 'bad':
-            curr_tag = 0
-        else:
-            print('error')
-        context.append(curr_tag)
-        context.append(w2i['start'])
-        for i in range(len(curr_word)):
-            context.append(w2i[curr_word[i]])
-        context.append(w2i['end'])
-        contexts.append(context)
-    return contexts
+# TODO load the data
 
+m = dy.Model()
+trainer = dy.AdamTrainer(m)
+embeds = m.add_lookup_parameters((VOCAB_SIZE, EMBED_SIZE))
+acceptor = LstmAcceptor(EMBED_SIZE, 100, 3, m)
 
-def make_indexes_to_data(data):
-    # strings to IDs
-    L2I = {l: i for i, l in enumerate(data)}
-    I2L = {i: l for l, i in L2I.iteritems()}
-    return L2I, I2L
+# training code
+sum_of_losses = 0.0
+for epoch in range(EPOCH):
+    # TODO change to train data
+    for sequence, label in [((1, 4, 5, 1), 1), ((42, 1), 2), ((56, 2, 17), 1)]:
+        dy.renew_cg()  # new computation graph
+        vecs = [embeds[i] for i in sequence]
+        preds = acceptor(vecs)
+        loss = dy.pickneglogsoftmax(preds, label)
+        sum_of_losses += loss.npvalue()
+        loss.backward()
+        trainer.update()
+    # TODO change 3 to len(train_data)
+    print sum_of_losses / 3
+    sum_of_losses = 0.0
 
-
-# return loss for one word
-def do_one_word(model, word, tag):
-    # setup the sentence
-    dy.renew_cg()
-    s0 = lstm.initial_state()
-
-    s = s0
-
-    for char in word:
-        s = s.add_input(lookup[char])
-
-    # MLP
-    W = dy.parameter(W_mlp)
-    b = dy.parameter(b_mlp)
-
-    probs = dy.softmax(W * s.output() + b)
-
-    # loss of the model
-    loss = -dy.log(dy.pick(probs, tag))
-    loss.backward()
-    trainer.update()
-    return loss
-
-
-# make prediction
-def make_prediction(model, word, tag):
-    # setup
-    dy.renew_cg()
-    s0 = lstm.initial_state()
-
-    W = dy.parameter(W_mlp)
-    bias = dy.parameter(b_mlp)
-    s = s0
-    for char in word:
-        s = s.add_input(lookup[char])
-
-    # MLP after LSTM outputs
-    probs = dy.softmax(W * s.output() + bias)
-
-    prediction = np.argmax(probs.npvalue())
-    return prediction
-
-
-if __name__ == '__main__':
-
-    train = read_data('train')
-    test = read_data('test')
-    c2i, i2c = make_indexes_to_data(vocab)
-
-    contexts = make_data_context(train, c2i)
-    contexts_test = make_data_context(test, c2i)
-
-    trainer = dy.AdagradTrainer(model)
-
-    for epoch in range(num_epochs):
-        print 1
-        for line in contexts:
-            tag, word = line[0], line[1:]
-            loss = do_one_word(model, word, tag)
-            loss_value = loss.value()
-            loss.backward()
-            trainer.update()
-        print 2
-        correct_answers = 0.0
-        for line in contexts_test:
-            tag, word = line[0], line[1:]
-            # setup the sentence
-            prediction = make_prediction(model, word, tag)
-            if tag == prediction:
-                correct_answers += 1
-        print(correct_answers / len(contexts_test))
-        print(correct_answers)
-        print(len(contexts_test))
+print "\n\nPrediction time!\n"
+# prediction code:
+# TODO change to test data
+for sequence in [(1, 4, 12, 1), (42, 2), (56, 2, 17)]:
+    dy.renew_cg()  # new computation graph
+    vecs = [embeds[i] for i in sequence]
+    preds = dy.softmax(acceptor(vecs))
+    vals = preds.npvalue()
+    print np.argmax(vals), vals
